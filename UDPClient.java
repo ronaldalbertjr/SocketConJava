b51e8d3710fdb4e78d9718af86965aac41c055fe
ronaldalbertjr <ronaldalbert1609@gmail.com>
2020-02-05 07:30:19 -0300

    Implemented UDP Connection

---
+++ UDPClient.java
@@ -0,0 +1,31 @@
+import java.net.*;
+import java.io.*;
+
+class UDPClient
+{
+   public static void main(String args[]) throws Exception
+   {
+      String serverName = args[0];
+      int port = Integer.parseInt(args[1]);
+
+      try{
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+        DatagramSocket clientSocket = new DatagramSocket();
+        InetAddress IPAddress = InetAddress.getByName(serverName);
+        byte[] sendData = new byte[1024];
+        byte[] receiveData = new byte[1024];
+        String sentence = inFromUser.readLine();
+        sendData = sentence.getBytes();
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+        clientSocket.send(sendPacket);
+        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+        clientSocket.receive(receivePacket);
+        String modifiedSentence = new String(receivePacket.getData());
+        System.out.println("FROM SERVER:" + modifiedSentence);
+        clientSocket.close();
+      }
+      catch(IOException e){
+        e.printStackTrace();
+      }
+   }
+}
